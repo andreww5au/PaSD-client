a9cab465693493b28442813dfff5aec50c023b78
Andrew Williams <Andrew.Williams@curtin.edu.au>
2021-12-17 10:43:33 +0800

    Update PaSD station code for fieldtest. Change postgres table names to start with pasd_ so they can be added to the MWA schedule database without confusion.

---
+++ database/fix_perms.sql
@@ -0,0 +1,23 @@
+alter table pasd_antenna_portmap owner to pasd;
+alter table pasd_fibre_portmap owner to pasd;
+alter table pasd_smartbox_state owner to pasd;
+alter table pasd_fndh_state owner to pasd;
+alter table pasd_smartbox_port_status owner to pasd;
+alter table pasd_fndh_port_status owner to pasd;
+alter table pasd_stations owner to pasd;
+
+grant select on pasd_antenna_portmap to mwaschedule, mwa_ro, mwa, tapuser, mwacode, PUBLIC;
+grant select on pasd_fibre_portmap to mwaschedule, mwa_ro, mwa, tapuser, mwacode, PUBLIC;
+grant select on pasd_smartbox_state to mwaschedule, mwa_ro, mwa, tapuser, mwacode, PUBLIC;
+grant select on pasd_fndh_state to mwaschedule, mwa_ro, mwa, tapuser, mwacode, PUBLIC;
+grant select on pasd_smartbox_port_status to mwaschedule, mwa_ro, mwa, tapuser, mwacode, PUBLIC;
+grant select on pasd_fndh_port_status to mwaschedule, mwa_ro, mwa, tapuser, mwacode, PUBLIC;
+grant select on pasd_stations to mwaschedule, mwa_ro, mwa, tapuser, mwacode, PUBLIC;
+
+grant usage on sequence pasd_antenna_portmap to pasd;
+grant usage on sequence pasd_fibre_portmap to pasd;
+grant usage on sequence pasd_smartbox_state to pasd;
+grant usage on sequence pasd_fndh_state to pasd;
+grant usage on sequence pasd_smartbox_port_status to pasd;
+grant usage on sequence pasd_fndh_port_status to pasd;
+grant usage on sequence pasd_stations to pasd;
