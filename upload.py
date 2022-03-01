2965603d70d12668a73cdb0c27f4dd2185a84846
Andrew Williams <Andrew.Williams@curtin.edu.au>
2022-03-01 16:37:24 +0800

    bugfix

--- upload.py
+++ upload.py
@@ -6,10 +6,20 @@ import os
 import socket
 import sys
 
+LOGFILE = 'upload.log'
+
+fh = logging.FileHandler(filename=LOGFILE, mode='w')
+fh.setLevel(logging.DEBUG)  # All log messages go to the log file
+sh = logging.StreamHandler()
+sh.setLevel(logging.INFO)  # Some or all log messages go to the console
+# noinspection PyArgumentList
+logging.basicConfig(handlers=[fh, sh],
+                    level=logging.DEBUG,
+                    format='%(levelname)s:%(name)s %(created)14.3f - %(message)s')
+
 from pasd import transport
 from pasd import command_api
 
-LOGFILE = 'upload.log'
 
 if socket.gethostname().startswith('orthrus'):
     DEFHOST = 'pasd-fndh2.mwa128t.org'
@@ -27,14 +37,7 @@ if __name__ == '__main__':
                         help='TCP port number to use')
     parser.add_argument('--address', dest='address', default=0,
                         help='Modbus address')
-    parser.add_argument('--debug', dest='debug', default=False, action='store_true',
-                        help='If given, drop to the DEBUG log level, otherwise use INFO')
     args = parser.parse_args()
-
-    if args.debug:
-        loglevel = logging.DEBUG
-    else:
-        loglevel = logging.INFO
 
     if (args.host is None) and (args.device is None):
         args.host = DEFHOST
@@ -52,15 +55,6 @@ if __name__ == '__main__':
     else:
         print('Filename must start with "FNPC" (and go to address 31 or 101), or "SBox" (and go to address 1-24).')
         sys.exit(-1)
-
-    fh = logging.FileHandler(filename=LOGFILE, mode='w')
-    fh.setLevel(logging.DEBUG)   # All log messages go to the log file
-    sh = logging.StreamHandler()
-    sh.setLevel(loglevel)        # Some or all log messages go to the console
-    # noinspection PyArgumentList
-    logging.basicConfig(handlers=[fh, sh],
-                        level=logging.DEBUG,
-                        format='%(levelname)s:%(name)s %(created)14.3f - %(message)s')
 
     if args.address == 0:
         print('Must supply a modbus address to send the new firmware to')
