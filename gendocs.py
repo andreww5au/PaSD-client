e9e6ba7fe7d62f7343fc9be6a69c470f52233bf0
andrew <Andrew.Williams@curtin.edu.au>
2024-08-12 12:06:34 +0800

    Update documentation generation and docs for PasD-Client

--- gendocs.py
+++ gendocs.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 
-import glob
 import os
+import pydoc
 
 """
 Generate the code documentation, using pydoc.
 """
 
 for dirname in ['pasd', 'sid', 'simulate']:
-    flist = glob.glob('%s/*.py' % dirname)
-    for fname in flist:
-        if '__init__' not in fname:
-            os.system('pydoc -w %s' % fname)
-            bname = os.path.splitext(os.path.basename(fname))[0]  # eg 'smartbox'
-            os.system('mv %s.html %s.%s.html' % (bname, dirname, bname))
-    os.system('pydoc -w %s' % dirname)
+    pydoc.writedocs(dirname, pkgpath='%s.' % dirname)
+    pydoc.writedoc(dirname)
 os.system('mv *.html docs')
